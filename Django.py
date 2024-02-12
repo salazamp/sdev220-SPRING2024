f0c2c0a25ffbeea51fc92dc8cc89872a4dc7d04b
salazamp <152572336+salazamp@users.noreply.github.com>
2024-02-12 00:22:12 -0500

    Update Django.py

--- Django.py
+++ Django.py
@@ -8,3 +8,20 @@ class book(models.Model):
   price = models.DecimalField(max_digits=6, decimal_places=2)
   def __str__(self):
     return self.title
+    # we need to create a view that will render our HTML template with 
+    from Django.shortcuts import render
+    from.models import book
+
+def book_list(request):
+  books = Book.objects.all()
+  return render(request, 'books/book_list.html', {'books':}
+                                                  
+  #create the HTML template. Create a folder called templates 
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookstore</title>
+</head>
+  
